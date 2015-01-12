638a58f8d68941122adc3593bf511ac0c16c3cbb
Jeroen Baert <jerbae@gmail.com>
2015-01-12 11:27:52 +0100

    Updated auth

--- josbot.py
+++ josbot.py
@@ -1,7 +1,18 @@
-import time, sys
+import tweepy, time, sys
+
+# Init twitter credentials
+execfile('credentials.py')
 
 def main():
-	print "Hello world, from the main function"
+	print "Josbot starting..."
+	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+  	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+	api = tweepy.API(auth)
+
+def readquotes():
+	
+	
+
 
 if __name__ == "__main__": main()
 
