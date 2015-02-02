4e7a225d859dc9f66452322d479b5a99e8cbcdd4
Jeroen Baert <jerbae@gmail.com>
2015-02-02 21:54:30 +0100

    Switched to YAML for settings saving/loading

--- josbot.py
+++ josbot.py
@@ -1,45 +1,51 @@
-import tweepy, time, sys, pickle, os.path, json
+# standard libs
+import time, sys, os.path
+# additional libs
+import yaml, tweepy
 
 # GLOBALS
-execfile('credentials.py')
+settings = {}
 
+# Main program loop
 def main():
 	print "Josbot starting..."
-	writeSampleConfig()
-	tweet_index = importTweetIndex()
+	loadSettings("settings.yml")
 	api = setupAuth()
 
+# Setup tweepy twitter auth, return api object
 def setupAuth():
 	print "Setting up Twitter auth"
-	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-  	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+	auth = tweepy.OAuthHandler(settings['CONSUMER_KEY'], settings['CONSUMER_SECRET'])
+  	auth.set_access_token(settings['ACCESS_KEY'], settings['ACCESS_SECRET'])
 	api = tweepy.API(auth)
 	return api
 
-# To remember which quote we last tweeted, the bot persists its last tweet index to a JSON file
-# This allows us to easy add new quotes to the quotes.txt
-# Returns the last quote that was tweeted index
-def importTweetIndex():
-	print "Importing tweet index"
-	tweet_index = [0]
-	if os.path.exists("tweetindex.txt"):
-		tweet_index = json.load(open("tweetindex.txt", "r"))
-	else:
-		json.dump(tweet_index, open("tweetindex.txt", "w"))
-	print "Current tweet index: " + str(tweet_index)
-	return tweet_index;
+# Load settings from yaml file (if fail: write sample config)
+def loadSettings(filename):
+	if os.path.exists("settings.yml"):
+		global settings
+		settings = yaml.load(open(filename, "r"))
+		print "Settings loaded"
+		print settings
 		
-#def readquotes():
-#	api.
+	else:
+		writeSampleConfig(filename)
+		print "No settings file found - wrote a sample config to "+filename
+		print "Provide the required settings and re-run the program."
+		quit()
 
-def writeSampleConfig():
+def saveSettings(filename):
+	yaml.dump(settings, open(filename, "w"), default_flow_style=False)
+		
+# Write a sample config
+def writeSampleConfig(filename):
 	settings = { 
-	"CONSUMER_KEY" : "KEYHERE",
-	"CONSUMER_SECRET" : "SECRETHERE",
-	"ACCESS_KEY" : "KEYHERE",
-	"ACCESS_SECRET" : "SECRETHERE"
+	"CONSUMER_KEY":"KEYHERE",
+	"CONSUMER_SECRET":"SECRETHERE",
+	"ACCESS_KEY":"KEYHERE",
+	"ACCESS_SECRET":"SECRETHERE",
+	"tweetindex":0
 	}
-	json.dump(settings, open("settings.json", "w"), sort_keys=True, indent=4)
+	yaml.dump(settings, open(filename, "w"), default_flow_style=False)
 	
 if __name__ == "__main__": main()
-
