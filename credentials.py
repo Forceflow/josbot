433669ccd39890b8fd54fba42626d141c2f28171
Jeroen Baert <jerbae@gmail.com>
2015-02-04 08:33:37 +0100

    Removed obsolete credentials file (is in yaml now)

--- credentials.py
+++
@@ -1,5 +0,0 @@
-# enter the corresponding information from your Twitter application:
-CONSUMER_KEY = 'key' # keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = 'secret' # keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = 'key' # keep the quotes, replace this with your access token
-ACCESS_SECRET = 'secret' #keep the quotes, replace this with your access token secret
