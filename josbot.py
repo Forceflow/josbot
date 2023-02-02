60556d73ce68aebc193e22c985aa8da0678536c1
Jeroen Baert <3607063+Forceflow@users.noreply.github.com>
2023-02-02 20:56:48 +0100

    Reworked main loop logic

--- josbot.py
+++ josbot.py
@@ -1,136 +1,129 @@
-# JosBot v0.5
+# JosBot v0.6
 # Author: @jbaert
 
-VERSION="0.5"
+VERSION="0.6"
 
 # standard python libs
 import random, time, sys, os.path, io, threading
 # additional libs (get these from your OS repositories or pip)
 import yaml, tweepy
 
+PREFIX="Josbot   :"
+PREFIX_TW="Twitter  :"
+PREFIX_MA="Mastodon :"
+
 # CONFIG
 SLEEPMIN = 86400 # At least 24 hrs between tweets (60*60*24)
 SLEEPMAX = 172800 # At max 48 hrs between tweets (60*60*48)
 
 # GLOBALS
-settings = {}
 settings_file = "settings.yml"
-quotes = []
 quotes_file = "quotes.txt"
+settings = {}
+quotes = {}
 
-# DISABLE TWEETS
-dry_run_twitter = False
-
+# MAIN PROGRAM LOOP
 def main():
-	print("Josbot", VERSION, "starting, loading settings from", settings_file)
+	print(PREFIX, "Version", VERSION, "starting")
 	
-	# Grab settings from file
-	loadSettings(settings_file)
+	# Load settings and quotes
+	loadSettings()
+	loadQuotes()
 
-	# Load quotes from file
-	global quotes
-	quotes = loadQuotes(quotes_file)
+	# Check if we're doing a dry run
+	if settings["dry_run_twitter"]:
+		print(PREFIX_TW, "Dry run, not really posting to Twitter. FOR TESTING PURPOSES.")
+	if settings["dry_run_mastodon"]:
+		print(PREFIX_MA, "Dry run, not really posting to Mastodon. FOR TESTING PURPOSES.")
 	
 	# Setup Twitter AUTH and FOLLOWBACK THREAD
-	if dry_run_twitter:
-		print("Started in DRY RUN mode: No interaction with Twitter API will be made.")
-		api = "NULL"
-	else:
+	api = "NULL"
+	if not settings["dry_run_twitter"]:
 		api = setupTwitterAuth()
-		followback_thread = threading.Thread(target=threaded_followBack, args=(api,dry_run_twitter))	
+		if settings["followback_twitter"]:
+			twitter_followback_thread = threading.Thread(target=twitterFollowBackThread, args=(api,))
 
-	# Main loop
-	print("Starting main loop")
+	print(PREFIX, "End of setup")
+
+	# MAIN LOOP
+	print(PREFIX, "-------------------------------")
+	print(PREFIX, "Starting main loop")
 	while True:
 		if int(settings["quote_index"]) < len(quotes):
-			
-			#if not dry_run_twitter:
-			#	manageTwitterFollowBackThread(api, followback_thread)
-			
 			# SLEEP
-			#sleeptime = random.randint(SLEEPMIN,SLEEPMAX)
-			#m, s = divmod(sleeptime, 60) # An alternate syntax when dealing with multiple return values is to have Python "unwrap" the tuple 
-			#h, m = divmod(m, 60)
-			#print("Going to sleep for %d:%02d:%02d" % (h, m, s))
-			time.sleep(600)
+			sleeploop()
 
 			# GET CURRENT QUOTE
 			current_quote_index=int(settings["quote_index"])
 			current_quote=quotes[current_quote_index]
+			print(PREFIX, "Current quote:", current_quote, "[", current_quote_index,"]")
 
-			if dry_run_twitter:
-				print("Dry run tweet:", current_quote)
-
-			# TWEET CURRENT QUOTE
-			if not dry_run_twitter:
-				tweetQuote(api,current_quote)
+			# TWEET
+			if not settings["dry_run_twitter"]:
+				tweetQuote(api, current_quote)
+			else:
+				print(PREFIX_TW, "(DRY RUN) Tweeting:", current_quote)
 
 			# INCREMENT QUOTE INDEX
 			settings["quote_index"] = current_quote_index + 1
-			saveSettings(settings_file)
+			saveSettings()
+
+			# MANAGE TWITTER FOLLOWBACK THREAD
+			if not settings["dry_run_twitter"] and settings["followback_twitter"]:
+				manageTwitterFollowBackThread(api, twitter_followback_thread)
 
 		else:
-			print("Ran out of quotes ... shuffle and restart")
-			resetQuotes(quotes_file)
+			print(PREFIX, "Ran out of quotes ... shuffling and restarting")
+			resetQuotes()
 			settings["quote_index"] = 0
-			saveSettings(settings_file)
+			saveSettings()
+
+# Function to go to sleep for a random period between SLEEPMIN and SLEEPMAX
+def sleeploop():
+	# Pick random sleeptime
+	sleeptime = random.randint(SLEEPMIN,SLEEPMAX)
+	# Print sleeptime in human readable form
+	m, s = divmod(sleeptime, 60) # An alternate syntax when dealing with multiple return values is to have Python "unwrap" the tuple 
+	h, m = divmod(m, 60)
+	print(PREFIX, "Going to sleep for %d:%02d:%02d" % (h, m, s))
+	# Do the sleeps
+	time.sleep(sleeptime)
 
 # Setup tweepy twitter auth, return api object
 def setupTwitterAuth():
-	print("Setting up Twitter auth")
+	print(PREFIX_TW, "Setting up Twitter auth")
 	auth = tweepy.OAuthHandler(settings['CONSUMER_KEY'], settings['CONSUMER_SECRET'])
 	auth.set_access_token(settings['ACCESS_KEY'],settings['ACCESS_SECRET'])
 	api = tweepy.API(auth, wait_on_rate_limit=True)
 	return api
-
-# Load settings from file (if fail: write sample config)
-def loadSettings(filename):
-	if os.path.exists(filename):
-		global settings
-		settings = yaml.load(open(filename, "r"), Loader=yaml.BaseLoader)
-		print("Settings loaded")
-	else:
-		writeSampleConfig(filename)
-		print("No settings file found - wrote a sample config to ", filename)
-		print("Provide the required settings and re-run the program.")
-		quit()
-
-def manageTwitterFollowBackThread(api, followback_thread):
-	if not followback_thread.is_alive():
-			followback_thread = threading.Thread(target=threaded_followBack, args=(api,dry_run_twitter)) # create new thread
-			print("Launching Followback Thread")
-			followback_thread.start()
-	else:
-			print("Followback thread already running")
 
 # Tweet a quote given by tweetindex in settings to a certain api
 def tweetQuote(api, quote):
 	print("Tweeting quote :", quote)
 	api.update_status(quote)
 
-# Follow all our followers back (threaded)
-def threaded_followBack(api, dry_run_twitter):
-	if(dry_run_twitter):
-		print("(FOLLOWBACK_THREAD) DRY RUN: Following all followers")
-		return
+# Manage the TwitterFollowBackThread: start it when it's not running
+def manageTwitterFollowBackThread(api, thread):
+	if not thread.is_alive():
+			print(PREFIX_TW, "Launching FollowbackThread")
+			thread.start()
 	else:
-		print("(FOLLOWBACK_THREAD) Following all our followers")
-	# Actual follow code
+			print(PREFIX_TW, "FollowbackThread already running")
+
+# Follow all our followers back (threaded)
+def twitterFollowBackThread(api):
+	print(PREFIX_TW, "Followback Thread: Following all our followers")
 	count = 0
 	for follower in tweepy.Cursor(api.get_followers).items():
 		try:
-			print("(FOLLOWBACK_THREAD) Trying to follow", follower.screen_name)
+			print(PREFIX_TW, "Followback Thread: Trying to follow", follower.screen_name)
 			follower.follow()
 			count += 1
 		except tweepy.errors.TweepyException as e:
-			print("(FOLLOWBACK_THREAD): ", e)
-			print("(FOLLOWBACK_THREAD) Hit a limit during following, sleeping for 20mins")
+			# print(PREFIX_TW, "Followback Thread:", e)
+			print(PREFIX_TW, "Followback Thread: Hit a limit during following, sleeping for 20mins")
 			time.sleep(60*60)
-	print("(FOLLOWBACK_THREAD) FINISHED: Followed", count, "people")
-
-# Save settings to file
-def saveSettings(filename):
-	yaml.dump(settings, open(filename, "w"), default_flow_style=False)
+	print(PREFIX_TW, "Followback Thread: Finished: Followed", count, "accounts")
 
 # Write a sample config
 def writeSampleConfig(filename):
@@ -139,21 +132,42 @@ def writeSampleConfig(filename):
 	"CONSUMER_SECRET":"SECRETHERE",
 	"ACCESS_KEY":"KEYHERE",
 	"ACCESS_SECRET":"SECRETHERE",
+	"dry_run_twitter": False,
+	"dry_run_mastodon": False,
+	"followback_twitter": False,
+	"followback_mastodon": False,
 	"quote_index":0
 	}
 	yaml.dump(settings, open(filename, "w"), default_flow_style=False)
 
+# Load settings from file (if fail: write sample config and quit)
+def loadSettings():
+	if os.path.exists(settings_file):
+		global settings
+		settings = yaml.safe_load(open(settings_file, "r"))
+		print(PREFIX, "Loaded", len(settings), "settings from", settings_file)
+	else:
+		writeSampleConfig(settings_file)
+		print(PREFIX, "No settings file found - wrote a sample config to ", settings_file)
+		print(PREFIX, "Provide the required settings and re-run the program.")
+		quit()
+
+# Save settings to file
+def saveSettings():
+	yaml.dump(settings, open(settings_file, "w"), default_flow_style=False)
+
 # Load and sanitize quotes from utf-8 txt file
-def loadQuotes(filename):
-	with io.open(filename, mode="r", encoding="utf-8") as f:
+def loadQuotes():
+	with io.open(quotes_file, mode="r", encoding="utf-8") as f:
 		content = [line.rstrip('\n').strip()[0:140] for line in f]
-	print("Loaded", str(len(content)), "quotes from", quotes_file)
-	return content
+	global quotes
+	quotes = content
+	print(PREFIX, "Loaded", str(len(content)), "quotes from", quotes_file)
 
 # Shuffle quotes and write them back to file
-def resetQuotes(filename):
+def resetQuotes():
 	random.shuffle(quotes)
-	file = io.open(filename, mode="w", encoding="utf-8")
+	file = io.open(quotes_file, mode="w", encoding="utf-8")
 	file.truncate(0)
 	for quote in quotes:
 		file.write("%s\n" % quote)
