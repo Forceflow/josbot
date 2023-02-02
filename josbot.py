6d27da0fdb1364021d99782bbe0695f873b124fd
Jeroen Baert <3607063+Forceflow@users.noreply.github.com>
2023-02-02 18:23:25 +0100

    Moved Twitter followback code to seperate functions

--- josbot.py
+++ josbot.py
@@ -19,55 +19,50 @@ quotes = []
 quotes_file = "quotes.txt"
 
 # DISABLE TWEETS
-dry_run = True
+dry_run_twitter = False
 
 def main():
 	print("Josbot", VERSION, "starting, loading settings from", settings_file)
+	
 	# Grab settings from file
 	loadSettings(settings_file)
-	# Setup AUTH
-	if dry_run:
+
+	# Load quotes from file
+	global quotes
+	quotes = loadQuotes(quotes_file)
+	
+	# Setup Twitter AUTH and FOLLOWBACK THREAD
+	if dry_run_twitter:
 		print("Started in DRY RUN mode: No interaction with Twitter API will be made.")
 		api = "NULL"
 	else:
 		api = setupTwitterAuth()
-	# Load quotes from file
-	global quotes
-	quotes = loadQuotes(quotes_file)
-
-	# Prepare followback thread
-	if not dry_run:
-		followback_thread = threading.Thread(target=threaded_followBack, args=(api,dry_run))
+		followback_thread = threading.Thread(target=threaded_followBack, args=(api,dry_run_twitter))	
 
 	# Main loop
 	print("Starting main loop")
 	while True:
 		if int(settings["quote_index"]) < len(quotes):
-			# TWITTER FOLLOWBACK
-			if not dry_run:
-				if not followback_thread.is_alive():
-					followback_thread = threading.Thread(target=threaded_followBack, args=(api,dry_run)) # create new thread
-					print("Launching Followback Thread")
-					followback_thread.start()
-				else:
-					print("Followback thread already running")
+			
+			#if not dry_run_twitter:
+			#	manageTwitterFollowBackThread(api, followback_thread)
 			
 			# SLEEP
 			#sleeptime = random.randint(SLEEPMIN,SLEEPMAX)
 			#m, s = divmod(sleeptime, 60) # An alternate syntax when dealing with multiple return values is to have Python "unwrap" the tuple 
 			#h, m = divmod(m, 60)
 			#print("Going to sleep for %d:%02d:%02d" % (h, m, s))
-			time.sleep(3)
+			time.sleep(600)
 
 			# GET CURRENT QUOTE
 			current_quote_index=int(settings["quote_index"])
 			current_quote=quotes[current_quote_index]
 
-			if dry_run:
-				print("Dry run:", current_quote)
+			if dry_run_twitter:
+				print("Dry run tweet:", current_quote)
 
 			# TWEET CURRENT QUOTE
-			if not dry_run:
+			if not dry_run_twitter:
 				tweetQuote(api,current_quote)
 
 			# INCREMENT QUOTE INDEX
@@ -100,14 +95,22 @@ def loadSettings(filename):
 		print("Provide the required settings and re-run the program.")
 		quit()
 
+def manageTwitterFollowBackThread(api, followback_thread):
+	if not followback_thread.is_alive():
+			followback_thread = threading.Thread(target=threaded_followBack, args=(api,dry_run_twitter)) # create new thread
+			print("Launching Followback Thread")
+			followback_thread.start()
+	else:
+			print("Followback thread already running")
+
 # Tweet a quote given by tweetindex in settings to a certain api
 def tweetQuote(api, quote):
 	print("Tweeting quote :", quote)
 	api.update_status(quote)
 
 # Follow all our followers back (threaded)
-def threaded_followBack(api, dry_run):
-	if(dry_run):
+def threaded_followBack(api, dry_run_twitter):
+	if(dry_run_twitter):
 		print("(FOLLOWBACK_THREAD) DRY RUN: Following all followers")
 		return
 	else:
@@ -116,7 +119,7 @@ def threaded_followBack(api, dry_run):
 	count = 0
 	for follower in tweepy.Cursor(api.get_followers).items():
 		try:
-			# print("(FOLLOWBACK_THREAD) Trying to follow", follower.screen_name)
+			print("(FOLLOWBACK_THREAD) Trying to follow", follower.screen_name)
 			follower.follow()
 			count += 1
 		except tweepy.errors.TweepyException as e:
